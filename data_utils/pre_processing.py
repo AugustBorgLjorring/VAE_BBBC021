6cda57aaa18297444c378fb3801c9c2f44ce2c58
AugustHolmDTU <s224166@dtu.dk>
2025-03-05 12:21:13 +0100

    Editing and added normalize function

--- data_utils/pre_processing.py
+++ data_utils/pre_processing.py
@@ -1,18 +1,36 @@
 import numpy as np
 
 
-def crop_image(image):
+def crop_image(image: np.ndarray, pix : int = 2) -> np.ndarray:
     """
-    Crops an image from (68, 68, 3) to (64, 68, 3) by removing
+    Params:
+    image: np.ndarray
+        An image with shape (68, 68, 3)
+
+    pix: int
+        Number of pixels to crop from the top and bottom of the image.
+
+    Crops an image from (68, 68, 3) to (64, 64, 3) by removing
     two pixels from the top and bottom.
 
     Returns the cropped image if shape matches, otherwise None.
     """
     if image.shape == (68, 68, 3):
-        return image[2:-2, :, :]
+        return image[pix:-pix, pix:-pix, :]
     else:
         print(f"Skipping image with unexpected shape: {image.shape}")
         return None
     
 
-    
+
+def normalize_image(image: np.ndarray) -> np.ndarray:
+    """
+    Normalizes an image within each channel.
+    im = (im - im.min()) / (im.max() - im.min())
+    """
+    min = image.min(axis=(0, 1))
+    max = image.max(axis=(0, 1))
+
+    image_norm = (image - min) / (max - min)
+
+    return image_norm
