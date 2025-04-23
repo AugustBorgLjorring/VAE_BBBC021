Build get_gradients function to measure the magnitude of gradients during training. Also adjusted the loss to include constant and have fixed sigma = 1.

src/vae_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class VAE(nn.Module):
     def __init__(self, input_channels=3, latent_dim=10):
@@ -59,7 +60,9 @@ class VAE(nn.Module):
         return x_recon, mu, log_var
 
     def loss_function(self, recon_x, x, mu, log_var):
-        recon_loss = F.mse_loss(recon_x, x, reduction='sum')
+        D = x.size(1) * x.size(2) * x.size(3) 
+        sigma = 1
+        recon_loss = 1/(2*sigma**2) * F.mse_loss(recon_x, x, reduction='sum') + 1/(2*sigma**2) * D * math.log(2 * math.pi)
         kld_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
         loss = recon_loss + kld_loss
         return loss, recon_loss, kld_loss